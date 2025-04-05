fc67a5783496e9db2e16e59db777e450693458f2
Joebus1 <106839320+Joebus1@users.noreply.github.com>
2025-04-04 23:29:35 -0400

    0.1.57

--- Assets/Scripts/Toys/Platform/PlatformToy.cs
+++ Assets/Scripts/Toys/Platform/PlatformToy.cs
@@ -1,62 +1,88 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-[DisallowMultipleComponent]
-public class PlatformToy : MonoBehaviour
+public class PlatformToy : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
-    public Transform platform;   // Main visual object
-    public Transform pivot;      // Center pivot point
-    public Transform leftHandle; // Left red tip
-    public Transform rightHandle;// Right red tip
+    public Transform platform;    // The white bar
+    public Transform pivot;       // Slightly below center
+    public Transform leftHandle;
+    public Transform rightHandle;
 
-    private bool draggingPlatform = false;
-    private bool draggingHandle = false;
+    private Camera mainCam;
+    private bool dragging = false;
+    private bool rotating = false;
     private Vector3 dragOffset;
-    private Transform draggedHandle;
+    private Transform activeHandle;
 
-    private void Update()
+    void Awake()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                if (hit.transform == leftHandle || hit.transform == rightHandle)
-                {
-                    draggingHandle = true;
-                    draggedHandle = hit.transform;
-                }
-                else if (hit.transform == platform)
-                {
-                    draggingPlatform = true;
-                    dragOffset = platform.position - hit.point;
-                }
-            }
-        }
+        mainCam = Camera.main;
+    }
 
-        if (Input.GetMouseButton(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                if (draggingHandle && draggedHandle != null)
-                {
-                    Vector3 direction = hit.point - pivot.position;
-                    direction.z = 0; // lock to 2D plane
-                    platform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
-                }
-                else if (draggingPlatform)
-                {
-                    Vector3 newPos = hit.point + dragOffset;
-                    newPos.z = platform.position.z;
-                    platform.position = newPos;
-                }
-            }
-        }
+    void Update()
+    {
+        // Always keep it Z-locked to 0 (2D look)
+        Vector3 pos = transform.position;
+        pos.z = 0;
+        transform.position = pos;
 
-        if (Input.GetMouseButtonUp(0))
+        Vector3 rot = transform.eulerAngles;
+        rot.x = 0;
+        rot.z = 0;
+        transform.eulerAngles = rot;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (IsPointerOnHandle(eventData, leftHandle))
         {
-            draggingHandle = false;
-            draggingPlatform = false;
+            rotating = true;
+            activeHandle = leftHandle;
         }
+        else if (IsPointerOnHandle(eventData, rightHandle))
+        {
+            rotating = true;
+            activeHandle = rightHandle;
+        }
+        else
+        {
+            dragging = true;
+            dragOffset = transform.position - WorldPoint(eventData.position);
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (rotating)
+        {
+            Vector3 dir = WorldPoint(eventData.position) - pivot.position;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            platform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+        else if (dragging)
+        {
+            transform.position = WorldPoint(eventData.position) + dragOffset;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        dragging = false;
+        rotating = false;
+        activeHandle = null;
+    }
+
+    private Vector3 WorldPoint(Vector2 screenPos)
+    {
+        return mainCam.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, mainCam.nearClipPlane));
+    }
+
+    private bool IsPointerOnHandle(PointerEventData eventData, Transform handle)
+    {
+        Vector2 screenPoint = eventData.position;
+        Vector2 handleScreenPos = mainCam.WorldToScreenPoint(handle.position);
+        float handleRadius = 40f; // Adjust to taste
+
+        return Vector2.Distance(screenPoint, handleScreenPos) < handleRadius;
     }
 }
